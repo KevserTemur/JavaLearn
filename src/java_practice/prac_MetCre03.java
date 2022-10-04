c76affb30afd86a60b8245b5febfa1adb6f7da43
Kevser Temür <kevsertemur@gmail.com>
2022-10-04 19:25:11 +0300

    kod düzeltildi.

--- src/java_practice/prac_MetCre03.java
+++ src/java_practice/prac_MetCre03.java
@@ -1,5 +1,7 @@
 package java_practice;
 
+import java.util.Scanner;
+
 public class prac_MetCre03 {
     /*
 Kullanicidan bir tam sayi alin.
@@ -9,5 +11,32 @@ uc basamakli veya daha buyukse birler,onlar ve yuzler basamagindaki rakamlarin t
 100'den kucukse sadece birler basamagini yazdirin.
 */
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("bir tam sayi giriniz :");
+        int sayi= sc.nextInt();
+        pozitifNegatif(sayi);
+        //System.out.println("sayi cift -> " +Q02_MethodCreation.isEven(sayi));
+        System.out.println(  sayi >= 100 ? (yuzdenBuyuk(sayi)) : (yuzdenKucuk(sayi)));
 
+
+
+    }
+
+    private static int yuzdenKucuk(int sayi) {
+        return sayi%10;
+
+    }
+
+
+
+    private static int yuzdenBuyuk(int sayi) {
+        int birler = sayi%10, onlar =(sayi/10) %10, yuzler = sayi/100;
+        int toplam =0;
+        toplam = birler + onlar+ yuzler;
+        return toplam;
+    }
+    private static void pozitifNegatif(int sayi) {
+        System.out.println(sayi >0 && sayi!=0 ? "pozitif":"negatif");
+    }
 }
