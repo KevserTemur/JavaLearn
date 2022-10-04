53b00a00c37f383da037aceab27b631b424a6627
Kevser Temür <kevsertemur@gmail.com>
2022-10-04 18:58:19 +0300

    kod düzeltildi

---
+++ src/java_practice/prac_MetCre03.java
@@ -0,0 +1,13 @@
+package java_practice;
+
+public class prac_MetCre03 {
+    /*
+Kullanicidan bir tam sayi alin.
+Bu sayinin negatif veya pozitif oldugunu,
+tek mi cift mi oldugunu ve
+uc basamakli veya daha buyukse birler,onlar ve yuzler basamagindaki rakamlarin toplamini,
+100'den kucukse sadece birler basamagini yazdirin.
+*/
+
+
+}
