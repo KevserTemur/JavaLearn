e3a7cc3b7ff07375ceeb492ca331403d18183542
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-10-24 18:49:15 +0300

    myconstructor

--- src/CodeForLife/sunu_PassBy/ArrayAsAReference.java
+++ src/CodeForLife/sunu_PassBy/ArrayAsAReference.java
@@ -1,11 +1,14 @@
 package CodeForLife.sunu_PassBy;
 
 public class ArrayAsAReference {
+
+
     public void changeTheArray (int [] array) {
 
-      //  array [0] = new int [] {7,8,9};
+        array [0] = new int [] {7,8,9};
 
         array [0] = array[0] +7;
+
 
         array [0] =95;
     }
