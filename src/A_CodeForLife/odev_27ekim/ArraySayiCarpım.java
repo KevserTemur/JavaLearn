c16e97757c26bfe1633636983ddbf427c98181f8
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-10-27 00:11:00 +0300

    ödev

--- src/A_CodeForLife/odev_27ekim/ArraySayiCarpım.java
+++ src/A_CodeForLife/odev_27ekim/ArraySayiCarpım.java
@@ -5,4 +5,14 @@ public class ArraySayiCarpım {
     //Soru 2) Bir integer array olusturunuz ve bu array’deki tum sayilarin carpimini For-each loop kullanarak bulunuz.
     // Sonucu ekrana yazdiriniz.
 
+    public static void main(String[] args) {
+        int sayi[] = {1,2,3,4,5,6};
+        int carpim = 1;
+        for (int value : sayi){
+            carpim*=value;
+        }
+        System.out.println(carpim);
+
+
+    }
 }
