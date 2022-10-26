43c85aaf68398ece5c60063c6de4bcffd175b530
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-10-27 00:28:07 +0300

    ödev

--- src/A_CodeForLife/odev_27ekim/Multidimensional_carpim.java
+++ src/A_CodeForLife/odev_27ekim/Multidimensional_carpim.java
@@ -4,6 +4,14 @@ public class Multidimensional_carpim {
 
     //Soru 4) Asagidaki multi dimensional array’in ic array’lerindeki son elemanlarin carpimini
     //ekrana yazdiran bir program yaziniz { {1,2,3}, {4,5}, {6} }
+    public static void main(String[] args) {
 
 
+        int sayiArr[][] ={{1,2,3}, {4,5}, {6}};
+        int crpm = 1;
+        for (int i = 0; i < sayiArr.length; i++) {
+            crpm*=sayiArr[i][sayiArr[i].length-1];
+        }
+        System.out.println(crpm);
+    }
 }
