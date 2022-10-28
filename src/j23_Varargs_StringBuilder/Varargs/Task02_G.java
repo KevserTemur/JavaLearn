f329e982e5125db19ba135897d7e3ed65c6e1abc
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-10-28 21:47:42 +0300

    varargs

---
+++ src/j23_Varargs_StringBuilder/Varargs/Task02_G.java
@@ -0,0 +1,33 @@
+package j23_Varargs_StringBuilder.Varargs;
+
+import java.util.Random;
+
+public class Task02_G {
+    private static Random sc;
+
+    public static void main(String[] args) {
+
+System.out.println("Lütfen bir sayı giriniz");
+    int sayi1=sc.nextInt();
+        System.out.println("Kaç tane daha sayı yazmak istersiniz");
+
+        System.out.println("toplaCarp()*sayi1 = " + toplaCarp() * sayi1);
+}
+
+public static int toplaCarp(int []... b) {
+
+    int sayiArr[] = new int[sc.nextInt()];
+    for (int i = 0; i < sayiArr.length; i++) {
+        System.out.println("Sayı giriniz");
+        sayiArr[i] = sc.nextInt();
+    }
+    int toplam = 0;
+    int carpım = 1;
+    for (int w : sayiArr) {
+        toplam += w;
+    }
+    return toplam;
+
+
+}
+}
