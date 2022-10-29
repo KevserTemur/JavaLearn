7abc652689ee9f26775d789c7667a781f571cfdf
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-10-29 13:44:57 +0300

    varargs

--- src/A_CodeForLife/Interview/Q02.java
+++ src/A_CodeForLife/Interview/Q02.java
@@ -1,5 +1,8 @@
 package A_CodeForLife.Interview;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
 public class Q02 {
     /*
     2 -->> Write a return method that can remove the duplicated values from string
@@ -15,19 +18,19 @@ Ex : removeDup("AAABBBCCC") ==> ABC
 
     } // main dışı
 
-    public static String removeDup (String str) {
+    public static String removeDup(String str) {
         String result = ""; // boş string açtık
 
-      // for (int i = 0; i < str.length(); i++) {
-      //     if (!result.contains(str.substring(i,i+1)));
-      //     result += str.substring(i, i+1);
-      // }
+        // for (int i = 0; i < str.length(); i++) {
+        //     if (!result.contains(str.substring(i,i+1)));
+        //     result += str.substring(i, i+1);
+        // }
 
 
         while (str.length() > 0) { // String boyunca devam etsin
-            if (!result.contains(str.substring(0,1))) { // resultta stringin birinci elemanı var mı yok mu
-                result += str.substring(0,1); // yoksa ekle
-                str = str.replaceAll(str.substring(0,1), ""); // ve olan her elemanı hiçlikle değiştir yani sil
+            if (!result.contains(str.substring(0, 1))) { // resultta stringin birinci elemanı var mı yok mu
+                result += str.substring(0, 1); // yoksa ekle
+                str = str.replaceAll(str.substring(0, 1), ""); // ve olan her elemanı hiçlikle değiştir yani sil
             }
         }
 
@@ -35,3 +38,5 @@ Ex : removeDup("AAABBBCCC") ==> ABC
 
     }
 }
+
+    // LinkedHashSet ile de yapılabiliyormuş. 21, dakika "collaction"
