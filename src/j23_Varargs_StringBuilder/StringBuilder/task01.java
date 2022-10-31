74aa5b20ad441840682d80a1b992c2f42be6c7a4
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-10-31 22:30:59 +0300

    StringBuilder

--- src/j23_Varargs_StringBuilder/StringBuilder/task01.java
+++ src/j23_Varargs_StringBuilder/StringBuilder/task01.java
@@ -10,6 +10,37 @@ Bir cümleyi parametre olarak kabul eden, StringBuilder kullanarak cümleyi ters
     Output: "Reversed sentence : avaJ evol I
        It is not a palindrome"
  */
+    public static void main(String[] args) {
 
+        System.out.println("tersString(\"ece\") = " + tersString("ece"));// ece
+        System.out.println("tersString(\"serhat\") = " + tersString("serhat"));// tahres
+
+        StringBuilder sb1=new StringBuilder("ali");
+
+        tersSB(sb1);//ila
+        tersSB(new StringBuilder("serhat"));//tahres
+        System.out.println("isPalindrom(\"ey edip adanada pide ye\") = " + isPalindrom("ey edip adanada pide ye"));
+        System.out.println("isPalindrom(\"aga\") = " + isPalindrom("aga"));//true
+        System.out.println("isPalindrom(\"nazım\") = " + isPalindrom("nazım"));//false
+    }//main sonu
+    public static String tersString(String str){//ali->ila--> amele code
+        String stringTersi="";
+
+        for (int i = str.length()-1; i >=0 ; i--) {
+            stringTersi+=str.charAt(i);
+        }
+        return  stringTersi;
+    }
+    public  static void tersSB(StringBuilder sb){//cıncıx code
+        System.out.println("sb.reverse() = " + sb.reverse());//sb ters ceviri
+    }
+
+    public  static boolean isPalindrom(String str){
+        if(str==null){
+            return false;
+        }
+        return new StringBuilder(str).reverse().toString().equals(str);
+
+    }
 
 }
