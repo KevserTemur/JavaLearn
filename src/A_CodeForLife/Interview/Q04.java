c23de3769918cea0c066f60b5504acb8393db1b5
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-02 13:37:50 +0300

    interview

--- src/A_CodeForLife/Interview/Q04.java
+++ src/A_CodeForLife/Interview/Q04.java
@@ -13,6 +13,9 @@ public class Q04 {
         System.out.println(reverse(str1)); // reverse metodu uygulandı
         System.out.println(reverse1(str1));
 
+        String st="abcde";
+        System.out.println(reverse(st));
+
     } // main sonu
 
     public static String reverse (String str){ // method oluşturuldu
@@ -26,9 +29,20 @@ public class Q04 {
 
 
 
+
+
     public static String reverse1(String str){
         return new StringBuffer(str).reverse().toString();
     }
 
-
+    public static String reverse2(String st){
+        char[] ch=st.toCharArray();
+        for (int i = 0; i < ch.length/2; i++) {
+            char temp=ch[i];
+            ch[i]=ch[ch.length-1-i];
+            ch[ch.length-1-i]=temp;
+        }
+        return new String(ch);
+    }
 }
+
