eef9208e93ce4c00101271572ed60a83a515b371
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-02 18:54:51 +0300

    interview

---
+++ src/A_CodeForLife/IlkUnique.java
@@ -0,0 +1,27 @@
+package A_CodeForLife;
+
+public class IlkUnique {
+
+    /*
+Bir Stringde ilk uniq karakteri bulup return eden method create ediniz.
+Ex:
+String str1="AAABBBCCCDEF";
+String uniqf="D";
+ */
+
+    public static void main(String[] args) {
+        System.out.println("uniq(\"AAABBBCCCDEF\") = " + uniq("AAABBBCCCDEF"));
+    }
+    public static String uniq(String str1){
+        String uniqbos="";
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.indexOf(str1.charAt(i))==str1.lastIndexOf(str1.charAt(i))){
+                uniqbos=String.valueOf(str1.charAt(i));break;
+            }
+        }
+        return uniqbos.length()==0?"Stringinizde uniq karakter bulunmamaktadir":uniqbos;
+
+    }
+
+
+}
