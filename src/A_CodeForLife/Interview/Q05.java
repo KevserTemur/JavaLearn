51986464628ebd26a8599bb484391464b3e5ad3c
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-03 11:38:57 +0300

    interview

--- src/A_CodeForLife/Interview/Q05.java
+++ src/A_CodeForLife/Interview/Q05.java
@@ -1,12 +1,22 @@
 package A_CodeForLife.Interview;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Q05 {
 
 
-   // 5 -->>  Write a return method that can find the frequency of characters
-   // Ex :  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+    // 5 -->>  Write a return method that can find the frequency of characters
+    // Ex :  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+    public static void main(String[] args) {
 
-
-
-
+    }
+    public static String unique(String str) {
+        String uni = "";
+        for (String w : str.split("")) {
+            uni += Collections.frequency(Arrays.asList(str.split("")), w) > 0 ? (uni.contains(w) ? "" :
+                    w + Collections.frequency(Arrays.asList(str.split("")), w)) : "";
+        }
+        return uni;
+    }
 }
