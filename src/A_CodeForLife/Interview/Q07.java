821b22e2728eacab07eeb33ccf5fe2aeb3caf9a4
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-03 21:53:02 +0300

    interview

--- src/A_CodeForLife/Interview/Q07.java
+++ src/A_CodeForLife/Interview/Q07.java
@@ -2,8 +2,23 @@ package A_CodeForLife.Interview;
 
 public class Q07 {
 
-   // 7  -->>  Write a return method that can the maximum number from an int Array
+
+   //  7  -->>  Write a return method that can the maximum number from an int Array
+
+    public static void main(String[] args) {
+
+        int [] arr = {52,36,98,13};
+        maxValue(arr);
+
+    }// main sonu
+    public static void maxValue (int [] arr){
+        int max = Integer.MAX_VALUE;
+        for (int each: arr) {
+            if (each < max);
+            max=each;
+        }
+        System.out.println(max);
+    }
 
 
 }
-
