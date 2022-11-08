1212c507871d9c4e3258aee35c2821743929dc86
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-08 18:51:55 +0300

    project AutomationMac

--- src/A_Projects/AutomationMac/Options.java
+++ src/A_Projects/AutomationMac/Options.java
@@ -5,12 +5,13 @@ public class Options {
 
     /*
     1) Kullanıcı ürünleri göstermeliyiz. ---->> ürün clasında bir obje toString()
-    2) Scanner ---->>  Scanner classından bir obje create ediniz.
+    2) Scanner ---->>  Scanner classından bir obje create ediniz.[productNum a set edilmeli. setProductNum()]
     3) Loop ---->>  Do-While loop ---->> kullanıcı 1-7 rakamları arasında bir seçim yapmalı
         ve eğer girmiş olduğu rakam 1 den küçük veya 7 den büyük ise döngü devam etmeli
         ve kullanıcı 1-7 arfasında bir seçim yapmaya force edilmeli
     4) 0 a basarak ta menüden çıkabilmeli
     5) kullanıcıdan productNum (1-7 arasında rakam {kullanıcı 1 seçerse kraker, 2 seçerse cips...etc})
+        -->> Switch-case yapısı kurmalıyız.
     6) switch benden ProductNum ve alınan bu ProductNum a göre ilgili ürünün price ı alınmalı getKraker(), getCips()...
     (price set edebilmek için ben price ın setPrice() methodunu kullanmalıyım..)
     7) return olarak bana price dönmeli. return getPrice() (double)
