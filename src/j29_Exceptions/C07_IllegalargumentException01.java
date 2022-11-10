d7f88ed5c5523e48b8d0de58a0e557d7739689fa
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-10 21:09:41 +0300

    Exceptions

--- src/j29_Exceptions/C07_IllegalargumentException01.java
+++ src/j29_Exceptions/C07_IllegalargumentException01.java
@@ -5,6 +5,20 @@ import java.util.Scanner;
 public class C07_IllegalargumentException01 {
 
     public static void main(String[] args) {
+        /*
+        ahan da TRİCK kosesinde bugun :)
+
+"throw" - throws" farkları
+1- "throws" -> main method kapanıs parantezi ile method body acılıs parantezi arasına yazılır.
+   "throw" -> method body içinde tanımlanır.
+2- "throws" -> keywordden sonra birden fazla exception tanımlanabilir
+   "throw" -> keywordden sonra sadece bir tane exception tanımlanır
+3- "throws" -> keywordden sonra sadece exception class name yazılır
+    "throw" ->  keywordden sonra exception obj tanımlanır
+4- "throws" -> keyword method ilk satırda sadece bir kez tanımlanır
+    "throw" -> keyword istenildiği kadar method body de tanımlanabilir.
+
+         */
         // IllegalargumentException -> kullanılmasını istenmeyen değerlerde pr hata vermesi isteniyorsa bu excp fırlatılıer
         // ornek : yas datası için negatif değer girilidğinde pr kırılmadan run olması ama girile değer için excp fırlatması için kullanılır..
 
