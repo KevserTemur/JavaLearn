2ead3de95ce823e84e68e31d9a95aa8ae54a3ec7
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-10 20:50:02 +0300

    Exceptions

---
+++ src/j29_Exceptions/C07_IllegalargumentException02.java
@@ -0,0 +1,15 @@
+package j29_Exceptions;
+
+public class C07_IllegalargumentException02 {
+
+    public static void main(String[] args) {
+
+
+
+    }// main sonu
+
+    public static void hata() {
+    }
+
+
+}
