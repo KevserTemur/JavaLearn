2b3517a10c75e6fad86f66ab9a83867383cabb1c
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-10 20:49:49 +0300

    Exceptions

--- src/j29_Exceptions/C07_IllegalargumentException01.java
+++ src/j29_Exceptions/C07_IllegalargumentException01.java
@@ -2,7 +2,7 @@ package j29_Exceptions;
 
 import java.util.Scanner;
 
-public class C07_IllegalargumentException {
+public class C07_IllegalargumentException01 {
 
     public static void main(String[] args) {
         // IllegalargumentException -> kullanılmasını istenmeyen değerlerde pr hata vermesi isteniyorsa bu excp fırlatılıer
@@ -12,11 +12,18 @@ public class C07_IllegalargumentException {
         System.out.print(" agam yasını giresen : ");
 
         int age = sc.nextInt();
+
+        // if (age<18){
+        //     System.out.println("yasınız : "+age+" ehliyet için uygun değil ");
+        //     throw new IllegalArgumentException();//throw komutu try-catch block olmadan da istenen method body'de tanımlanabilir.
+        // }
+        // else System.out.println("yasınız : "+age+" ehliyet için uygun");
+
         try {
             if (age < 18) {
-               throw new IllegalArgumentException();//throw keyword ile Excp obj olusturularak hata tanımlanır ve fırlatırlır
+                throw new IllegalArgumentException();//throw keyword ile Excp obj olusturularak hata tanımlanır ve fırlatırlır
                 // böylece sartımıza uymayan  değer için de catch block'a düşürerek daha pratik code yazıldı
-              //  System.out.println("ehliyete basvurman için daha  : " + (18 - age) + "yıl var ...");
+                //  System.out.println("ehliyete basvurman için daha  : " + (18 - age) + "yıl var ...");
             } else System.out.println("agam ehliyete başvurabilirsin hayırlı lsun  " + age);
             System.out.println("agam try block da bu yazıyı okuduysan excp fırlatmadı komut sorunsuz ");
         } catch (IllegalArgumentException e) {
