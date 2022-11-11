ca49db60b8d5110a440b21ed448c1ee9a08013d7
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-11 22:26:07 +0300

    Abstract

--- src/j32_Abstract/abstract01/Accord.java
+++ src/j32_Abstract/abstract01/Accord.java
@@ -1,4 +1,26 @@
 package j32_Abstract.abstract01;
 
-public class Accord {
+public class Accord extends Honda {
+    @Override
+    public void motor() { // implement edilen abs method
+        System.out.println("2,0 motorlu hız felakettir");
+
+    }
+
+    @Override
+    void koltuk() { // implement edilen abs method
+        System.out.println("deri koltur terletir");
+
+    }
+
+    @Override
+    void sunrooff() { // override edilen concrete method
+        System.out.println("çok havalı");
+    }
+
+    @Override
+    void kapı() { // implement edilen abs method
+        System.out.println("kapıları dikkatli kapat");
+
+    }
 }
--- src/j32_Abstract/abstract01/Civic.java
+++ src/j32_Abstract/abstract01/Civic.java
@@ -1,6 +1,18 @@
 package j32_Abstract.abstract01;
 
-public class Civic extends Honda {
+public class Civic extends Honda {// Honda parent abs class extend child concrete classs
+    // extend honda yazılarak Honda Classs a concrete bir Civic class tanımlandı
+    // java CTE verdi
+    // çözüm
+    // a- olarak unimplemented (uyarlanmış) method implement edilmeli
+    // b- parent Honda Class dan abstract keyword kaldırılmalı
+    // c- concrete olan child Civic classs abstract tanımlanmalı
+
+    //concrete class da abstract metyhod tanımlanır mı?
+
+
+   // public abstract void absMethod (); ya class abs olmalı ya da method concrete body olmalı
+
     @Override
     public void motor() {
         System.out.println("1,6 eco motor az yakar");
@@ -14,5 +26,9 @@ public class Civic extends Honda {
     @Override
     void kapı() {
         System.out.println("5 kapı sedan");
+    }
+
+    public int vites () { // concrete child class method
+        return 5;
     }
 }
