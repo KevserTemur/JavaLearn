16363982275fc8732c59fb268b09c27fd3fa463c
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-14 21:17:49 +0300

    Abstract

---
+++ src/j32_Abstract/abstract02/Personel.java
@@ -0,0 +1,21 @@
+package j32_Abstract.abstract02;
+
+public abstract class Personel {
+    String name = "nur hanım";
+
+    public abstract void maasHesapla();//abs. meth
+    public abstract void maasBilgisi();//abs. meth
+
+
+    public  void  sigorta(){
+        System.out.println("Agam özel sigorta kapsamındasın :) ");//conc. meth
+    }
+
+
+
+
+
+
+}
+
+
