4838a72a90d7fa1fd1346bf239c733a754aa46d1
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-15 21:14:10 +0300

    Abstract

---
+++ src/j32_Abstract/Ornek3/Salad.java
@@ -0,0 +1,10 @@
+package j32_Abstract.Ornek3;
+
+
+
+    public abstract class Salad extends Food {
+        @Override
+        public void taste() {
+            System.out.println("sirkeli, limonlu, sumaklı...");
+        }
+}
