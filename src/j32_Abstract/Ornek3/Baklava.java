b9be7e67750b1e49dd0cdae81da20f54f79a21d6
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-15 21:12:48 +0300

    Abstract

---
+++ src/j32_Abstract/Ornek3/Baklava.java
@@ -0,0 +1,10 @@
+package j32_Abstract.Ornek3;
+
+
+    public class Baklava extends Sweet {
+        @Override
+        public void madeIn() {
+            System.out.println("yerli ve milli baklava...");
+        }
+    }
+
