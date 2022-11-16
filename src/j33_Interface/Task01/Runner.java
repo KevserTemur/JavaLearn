97418f78c558f68cd91e1c28cedea32c639d8959
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-16 14:27:06 +0300

    Interface

---
+++ src/j33_Interface/Task01/Runner.java
@@ -0,0 +1,25 @@
+package j33_Interface.Task01;
+
+public class Runner {
+
+    /*
+    Kare, Dikdortken, ve cember sınıflarından oluşacak bir geometri programı yapılmak isteniyor.
+    bunların ayrı ayrı , fakat ortak metodlarının (cevresi() ve alani() )zorunlu olabilmesi için
+    gerekli yapıyı kurunuz ve print ediniz....
+     */
+
+    public static void main(String[] args) {
+        Cember c=new Cember();
+        System.out.println("c.alan(4) = " + c.alan(4));
+        System.out.println("c.cevre(4) = " + c.cevre(4));
+
+        Dikdortgen d=new Dikdortgen();
+        System.out.println("d.alan(4,5) = " + d.alan(4, 5));
+        System.out.println("d.cevre(4,5) = " + d.cevre(4, 5));
+        System.out.println("d.cevre(24,38) = " + d.cevre(24, 38));
+
+        Kare k=new Kare();
+        System.out.println("k.alan(5) = " + k.alan(5));
+        System.out.println("k.cevre(8) = " + k.cevre(8));
+    }
+}
