fb3e4eb34383dfec4c604b0f36e499330b2f36e7
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-18 14:34:56 +0300

    static variable

--- src/j14_Scope/C02_staticVariable.java
+++ src/j14_Scope/C02_staticVariable.java
@@ -4,11 +4,11 @@ public class C02_staticVariable {
 /*  instance variable'lar obje'ye bagimlidir ve her obje farkli degerler alabilir
         ogrenci notlari veya ogretmen branslari gibi
         bir objeye ait bir variable'in son degerini bulmak icin sadece o objeyi dikkate aliriz
-​
+
        Static variabler ise class variable olarak tanimlanir
        ve tum class uyeleri icin aynidir. Okul ismi, okul mudurunun adi gibi
        eger static variable'in degeri degistirilirse herkes icin degisir.-> güneş misali
-​
+
      */
 
     static String firmaName = "clarusway";
