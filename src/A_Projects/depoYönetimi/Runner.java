9fab1f1d593126de7ebbd7d0aa080b368a9b5694
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-25 15:05:34 +0300

    lambda

---
+++ src/A_Projects/depoYönetimi/Runner.java
@@ -0,0 +1,9 @@
+package A_Projects.depoYönetimi;
+
+    public class Runner {
+        public static void main(String[] args) {
+            Islemler depo1 = new Islemler();
+            depo1.menuYazdir();
+        }
+    }
+
