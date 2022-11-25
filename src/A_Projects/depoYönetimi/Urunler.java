216860941103d1828d846279a9e2c85496c8b733
kevser temür <114690557+KevserTemur@users.noreply.github.com>
2022-11-25 15:05:15 +0300

    lambda

---
+++ src/A_Projects/depoYönetimi/Urunler.java
@@ -0,0 +1,19 @@
+package A_Projects.depoYönetimi;
+
+
+    public class Urunler {
+        static int idVer = 1000;
+        String urunIsmı;
+        String uretici;
+        int miktar;//miktar methodda tanımlanacak
+        String birim;
+        String raf;//methodda tanımlanacak
+        int id;//otomatik olarak idVer 1 artılıp cons. da kendisi atayacak
+        public Urunler(String urunIsmı, String uretici, String birim) {
+            this.urunIsmı = urunIsmı;
+            this.uretici = uretici;
+            this.birim = birim;
+            this.id=idVer++;
+        }
+    }
+
