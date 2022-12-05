4100fc2d78e8525d6ee57197bf3e3e57bbf8f0d9
Pc <kevsertemur@gmail.com>
2022-12-05 18:48:45 +0300

    lambdaPractice

---
+++ src/alim.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class alim {
+    public static void main(String[] args) {
+
+     //  Scanner scan = new Scanner(System.in);
+     //  System.out.println("adın ne? ");
+     //  String ad = scan.nextLine();
+     //  System.out.println("merhaba bilge");
+
+
+ Scanner scan = new Scanner(System.in);
+
+     System.out.println("bir sayı giriniz: ");
+     int a = scan.nextInt();
+     System.out.println("bir sayı giriniz: ");
+     int b = scan.nextInt();
+
+     System.out.println("sonuç: " + (a+b));
+
+
+
+   //   Scanner scan = new Scanner(System.in);
+   // System.out.println("kaç yaşındasın: ");
+
+
+   // int yas = scan.nextInt();
+
+
+   // System.out.println(yas < 10 ? "tablet oynayamazsın, doğru yatağa" : "tablet oynayabilirsin");
+
+       }
+    }
