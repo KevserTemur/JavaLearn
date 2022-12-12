9a25d39b9b34cb235bb2ee98178c771b53278736
Pc <kevsertemur@gmail.com>
2022-12-12 22:01:41 +0300

    java eksikler

--- src/j02_DataTypes_WrapperClass/tasks/_05_create_boolean1.java
+++ src/j02_DataTypes_WrapperClass/tasks/_05_create_boolean1.java
@@ -4,13 +4,12 @@ public class _05_create_boolean1 {
 
     public static void main(String[] args) {
 
-
     /*    Değeri 'true' olan bir boolean oluşturunuz.
           Boolean'ı yazdırınız. */
 
         //Kodu aşağıya yazınız.
-boolean ogrenciMi=true;
-        System.out.println(ogrenciMi);
 
+        boolean a = true;
+        System.out.println(a);
     }
 }
